29eef65200833f931db3f3178ea45f1c5a7b92f8
Serhat <s.ciftci561@gmail.com>
2025-03-24 00:21:49 +0300

    Entites olusturuldu Migration İle Mssql baglantısı yapıldı

---
+++ SavoryCorner.WebApi/Entites/Image.cs
@@ -0,0 +1,9 @@
+﻿namespace SavoryCorner.WebApi.Entites
+{
+    public class Image
+    {
+        public int ImageId { get; set; }
+        public string Title  { get; set; }
+        public string ImageUrl  { get; set; }
+    }
+}
---
+++ SavoryCorner.WebApi/Entites/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace SavoryCorner.WebApi.Entites
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int ProductDescription { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
